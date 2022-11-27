f60775fb70ca1ab7ba3d5b935643337be428f399
WojtekKamilowski <kamil.wojciechowski105@gmail.com>
2022-11-27 22:38:20 +0000

    Add Event Listener DOMContentLoaded, edit js file name, change id of main-container on index.html to specifically target dish lottery elements

---
+++ assets/js/dishlottery.js
@@ -0,0 +1,12 @@
+/**
+ * Add an event listener to the document and run the meals screen with buttons to choose for which meal idea the user is looking for
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    runMealsScreen();
+});
+
+/**
+ * Set up of lottery variables to vary display/hide
+ */
+
+
