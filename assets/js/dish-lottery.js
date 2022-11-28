6aca0919a70d88cd29fd896fd3285c413879eb40
WojtekKamilowski <kamil.wojciechowski105@gmail.com>
2022-11-28 07:27:25 +0000

    Add runMealsScreen function

--- assets/js/dish-lottery.js
+++ assets/js/dish-lottery.js
@@ -12,3 +12,26 @@ document.addEventListener("DOMContentLoaded", function () {
  let mainMealsScreen = document.getElementById("main-container-meals");
  let mainDishScreen = document.getElementById("main-container-dishes");
 
+
+/**
+* Show the meals screen with buttons to choose for which meal idea the user is looking for
+*/
+function runMealsScreen() {
+    mainMealsScreen.style.display = "block";
+    mainDishScreen.style.display = "none";
+}
+
+/**
+ * Display a screen with the dish that was drawn and the quiz
+ */
+
+function runDishLottery() {
+    document.getElementById('main-container-meals').addEventListener('click', function (event) {
+        if (!event.target.className.includes("meal-box")) return; // prevent click over all div with three buttons
+        let button = event.target;
+        let dishLottery = button.getAttribute('data-type');
+        setGame(gameLevel);
+    });
+}
+
+
