1bd8584def0293dff94fef858f5771ca4508baa6
WojtekKamilowski <kamil.wojciechowski105@gmail.com>
2022-11-28 21:30:03 +0000

    Write code for the runDishLottery(mealType) function

--- assets/js/dish-lottery.js
+++ assets/js/dish-lottery.js
@@ -1,16 +1,16 @@
 /**
  * Add an event listener to the document and run the meals screen with buttons to choose for which meal idea the user is looking for
  */
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
     runMealsScreen();
-});
+})
 
 /**
  * Set up of lottery variables to vary display/hide
  */
 
- let mainMealsScreen = document.getElementById("main-container-meals");
- let mainDishScreen = document.getElementById("main-container-dishes");
+ let mainMealsScreen = document.getElementById('main-container-meals');
+ let mainDishScreen = document.getElementById('main-container-dishes');
 
 /**
 * Show the meals screen with buttons to choose for which meal idea the user is looking for
@@ -18,41 +18,24 @@ document.addEventListener("DOMContentLoaded", function () {
 function runMealsScreen() {
     mainMealsScreen.style.display = "block";
     mainDishScreen.style.display = "none";
-}
 
-/**
-* Show the dish screen with dish draw and quiz
-*/
-
-function runDishScreen() {
-    mainMealsScreen.style.display = "none";
-    mainDishScreen.style.display = "block";
-}
-
-/**
- * Display a screen with the dish that was drawn and the quiz
- */
-
-function selectDishLottery() {
-    document.getElementById('main-container-dishes').addEventListener('click', function (event) {
-        if (!event.target.className.includes("meal-box")) return; // prevent click over all div with three buttons
+    document.getElementById('meal-buttons').addEventListener("click", function(event) {
+        if (!event.target.className.includes('meal-box')) return;
         let button = event.target;
-        let mealDraw = button.getAttribute('data-type');
-        runLottery(mealDraw);
+        let mealType = button.getAttribute('data-type');
+        runDishLottery(mealType);      
     });
 }
 
 /**
- * Draw a dish idea for the selected meal.
- * Display selected meal.
- * Set up dishes to be drawn.
- * @param {string} mealDraw 
- */
-function runLottery(mealDraw) {
-    runDishScreen()
+* Run the dish lottery with dish draw and quiz
+*/
+
+function runDishLottery(mealType) {
+    mainMealsScreen.style.display = "none";
+    mainDishScreen.style.display = "block";
+
+    document.getElementById('meal-text').innerHTML = `Today for ${mealType} let's eat:`;
+    document.getElementById('dish-name').innerHTML = drawnDish();
 }
-
-// document.getElementById("meal-text").innerHTML = `Today for ${mealDraw} let's eat:`; let lotteryParameters = lotterySettings
-
-
 
