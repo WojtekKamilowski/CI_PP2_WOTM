c7a45dd428883d18aaa868239f0b2105e793f6a7
WojtekKamilowski <kamil.wojciechowski105@gmail.com>
2022-12-04 16:38:50 +0000

    Start showQuestion function

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -2,19 +2,26 @@ let randomQuestions, currentQuestionIndex;
 let questionElement = document.getElementById('quiz-question');
 let answerButtonsElement = document.getElementById('answers');
 
-
 function startQuiz() {
     randomQuestions = questions.sort(() => Math.random() - .5);
     currentQuestionIndex = 0
-    setNextQuestion();    
+    setQuestion();    
 }
 
-function setNextQuestion() {
+function setQuestion() {
     showQuestion(randomQuestions[currentQuestionIndex])  
 }
 
 function showQuestion(question) {
-    document.getElementById('quiz-question').innerHTML = question.question;
+    questionElement.innerHTML = question.question;
+    question.answers.forEach(answer => {
+        document.getElementsByClassName('quiz-answer').innerHTML = answer.text;
+        if (answer.correct) {
+            quizButton.dataset.correct = answer.correct
+        }
+        quizButton.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(quizButton)
+    })
 }
 
 function selectAnswer() {
