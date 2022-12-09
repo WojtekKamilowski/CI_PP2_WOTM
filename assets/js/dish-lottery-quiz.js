9f0657e23390c1e9a8ff1f4d639a1e34c5cd98ef
WojtekKamilowski <kamil.wojciechowski105@gmail.com>
2022-12-09 21:00:05 +0000

    Remove quiz.js and add code to renamed dish-lottery-quiz.js to improve performance

--- assets/js/dish-lottery-quiz.js
+++ assets/js/dish-lottery-quiz.js
@@ -74,4 +74,97 @@ function runDishLottery(mealType) {
     startQuiz() 
 };
 
+// quiz based on https://www.youtube.com/watch?v=riDzcEQbX6k
+
+let randomQuestions, currentQuestionIndex; // declared variables to be defined
+let questionElement = document.getElementById('quiz-question'); // quiz-question p
+let answerButtonsElement = document.getElementById('answers'); // div with possible answers for the quiz
+let nextButton = document.getElementById('next-btn'); // ask me again! button 
+
+nextButton.addEventListener('click', () => { // sets a random quiz after clicking ask me again! button
+    currentQuestionIndex++
+    setQuestion();
+    if (randomQuestions.length > currentQuestionIndex + 1) { //restarts the quiz after all questions have been displayed
+        startQuiz();
+    }    
+})
+
+/**
+* Starts quiz with a random question
+*/
+function startQuiz() {
+    randomQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    setQuestion();    
+}
+
+/**
+* Set up a next quiz question
+*/
+function setQuestion() {
+    resetQuestion();
+    showQuestion(randomQuestions[currentQuestionIndex]);  
+}
+
+/**
+* Displays the quiz question
+* Add answers for the question
+*/
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        let quizButton = document.createElement('button');
+        quizButton.innerText = answer.text;
+        quizButton.classList.add('quiz-answer');
+        if (answer.correct) {
+            quizButton.dataset.correct = answer.correct
+        };
+        quizButton.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(quizButton);
+    })
+}
+
+/**
+* Resets the quiz to default state everytime a new question is set
+*/
+function resetQuestion() {
+    clearStatusClass(document.getElementById('quiz'));
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    };
+}
+
+/**
+* Activates quiz results after clicking on an answer
+* Changes background color of quiz div (red for a wrong answer and green for correct answer)
+*/
+function selectAnswer(e) {
+    let selectedQuizButton = e.target;
+    let correct = selectedQuizButton.dataset.correct;
+    checkAnswer(document.getElementById('quiz'), correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        checkAnswer(button, button.dataset.correct);
+    });
+}
+
+/**
+*  Checks which answers is correct and which are not
+*/
+function checkAnswer(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong');
+    };
+}
+
+/**
+* Removes correct/ wrong classes before checking the answer
+*/
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
+}
+
 
